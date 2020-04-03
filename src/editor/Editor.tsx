dfd99a63912f5d0ec518c75091f65bade56bcdbe
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2020-04-03 17:49:31 +0200

    Implement crudely all required functionalities.

--- src/editor/Editor.tsx
+++ src/editor/Editor.tsx
@@ -7,17 +7,32 @@ import {
   RenderElementProps,
   RenderLeafProps
 } from "slate-react";
-
+import isUrl from 'is-url';
 import { DefaultElement } from "./elements";
-import { Toolbar } from "./Toolbar";
+import { Toolbar, wrapLink } from "./Toolbar";
 
 function renderElement(props: RenderElementProps) {
   const { attributes, children } = props;
   return <DefaultElement {...attributes}>{children}</DefaultElement>;
 }
 
-function renderLeaf(props: RenderLeafProps) {
-  const { attributes, children } = props;
+function renderLeaf({ attributes, children, leaf }: RenderLeafProps) {
+  if (leaf.type === 'link') {
+    children = <a href={leaf.url}>{children}</a>
+  }
+
+  if (leaf.bold) {
+    children = <strong>{children}</strong>
+  }
+
+  if (leaf.italic) {
+    children = <em>{children}</em>
+  }
+
+  if (leaf.underlined) {
+    children = <u>{children}</u>
+  }
+
   return <span {...attributes}>{children}</span>;
 }
 
@@ -31,11 +46,39 @@ export interface EditorProps {
 
 export function Editor(props: EditorProps) {
   const { value, onChange, ...other } = props;
-  const editor = React.useMemo(() => withReact(createEditor()), []);
+  const editor = React.useMemo(() => withReact(withLinks(createEditor())), []);
+
+  function withLinks(editor: any) {
+    /* const { insertData, insertText, isInline } = editor
+  
+    editor.isInline = (element: any) => {
+      return element.type === 'link' ? true : isInline(element)
+    }
+  
+    editor.insertText = (text: any) => {
+      if (text && isUrl(text)) {
+        wrapLink(editor, text)
+      } else {
+        insertText(text)
+      }
+    }
+  
+    editor.insertData = (data: any) => {
+      const text = data.getData('text/plain')
+  
+      if (text && isUrl(text)) {
+        wrapLink(editor, text)
+      } else {
+        insertData(data)
+      }
+    } */
+  
+    return editor
+  }
 
   return (
     <Slate editor={editor} value={value} onChange={onChange}>
-      <Toolbar open={true} />
+      <Toolbar/>
       <Editable
         renderElement={renderElement}
         renderLeaf={renderLeaf}
--- src/editor/Toolbar.tsx
+++ src/editor/Toolbar.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {
+  useState,
+  useEffect,
+  useRef
+} from "react";
 import {
-  Popper,
-  PopperProps,
   ButtonGroup,
   IconButton,
   Input
@@ -14,6 +16,8 @@ import {
   Close
 } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
+import { ReactEditor, useSlate } from 'slate-react';
+import { Range, Editor, Transforms, Text } from 'slate';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -38,59 +42,183 @@ const useStyles = makeStyles(theme => ({
     "&:hover": {
       opacity: 1
     }
+  },
+  container: {
+    padding: '8px 7px 6px',
+    position: 'absolute',
+    zIndex: 1,
+    top: '-10000px',
+    left: '-10000px',
+    marginTop: '-6px',
+    opacity: 0,
+    backgroundColor: '#222',
+    borderRadius: '4px',
+    transition: 'opacity 0.75s'
   }
 }));
 
-export interface ToolbarProps extends Omit<PopperProps, "children"> {}
+export interface ToolbarProps {}
 
 export function Toolbar(props: ToolbarProps) {
-  const [link, setLink] = React.useState('');
+  const [link, setLink] = useState('');
   const s = useStyles();
 
+  const ref : any = useRef()
+  const editor = useSlate()
+
+  useEffect(() => {
+    const el : any = ref.current
+    const { selection } = editor
+
+    if (!el) {
+      return
+    }
+
+    if (
+      !selection ||
+      !ReactEditor.isFocused(editor) ||
+      Range.isCollapsed(selection) ||
+      Editor.string(editor, selection) === ''
+    ) {
+      el.removeAttribute('style')
+      return
+    }
+
+    const domSelection = window.getSelection();
+
+    if (domSelection) {
+      const domRange = domSelection.getRangeAt(0)
+      const rect = domRange.getBoundingClientRect()
+      el.style.opacity = 1
+      el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`
+      el.style.left = `${rect.left +
+        window.pageXOffset -
+        el.offsetWidth / 2 +
+        rect.width / 2}px`
+    }
+  })
+
   return (
-    <Popper className={s.root} {...props}>
-      {link === null ? (
-        /* Formatting controls */
-        <ButtonGroup variant="text" color="primary">
-          <IconButton className={s.button} size="small">
-            <FormatBold fontSize="small" />
-          </IconButton>
-          <IconButton className={s.button} size="small">
-            <FormatItalic fontSize="small" />
-          </IconButton>
-          <IconButton className={s.button} size="small">
-            <FormatUnderlined fontSize="small" />
-          </IconButton>
-          <IconButton
-            className={s.button}
-            size="small"
-            onClick={() => setLink('')}
-          >
-            <Link fontSize="small" />
-          </IconButton>
-        </ButtonGroup>
-      ) : (
-        /* URL input field */
-        <form onSubmit={x => x.preventDefault()}>
-          <Input
-            className={s.input}
-            type="url"
-            value={link}
-            onChange={x => setLink(x.target.value)}
-            endAdornment={
-              <Close
-                className={s.close}
-                fontSize="small"
-                onClick={() => setLink('')}
-              />
-            }
-            placeholder="https://"
-            disableUnderline
-            fullWidth
-            autoFocus
-          />
-        </form>
-      )}
-    </Popper>
+    <div ref={ref} className={s.container}>
+        {!link ? (
+          /* Formatting controls */
+          <ButtonGroup variant="text" color="primary">
+            <IconButton className={s.button} size="small" onMouseDown={onBoldButtonMouseDown}>
+              <FormatBold fontSize="small" />
+            </IconButton>
+            <IconButton className={s.button} size="small" onMouseDown={onItalicButtonMouseDown}>
+              <FormatItalic fontSize="small" />
+            </IconButton>
+            <IconButton className={s.button} size="small" onMouseDown={onUnderlinedButtonMouseDown}>
+              <FormatUnderlined fontSize="small" />
+            </IconButton>
+            <IconButton className={s.button} size="small" onMouseDown={onLinkButtonMouseDown}>
+              <Link fontSize="small" />
+            </IconButton>
+          </ButtonGroup>
+        ) : (
+          /* URL input field */
+          <form onSubmit={x => x.preventDefault()}>
+            <Input
+              className={s.input}
+              type="url"
+              value={link}
+              onChange={x => setLink(x.target.value)}
+              endAdornment={
+                <Close
+                  className={s.close}
+                  fontSize="small"
+                  onClick={() => setLink('')}
+                />
+              }
+              placeholder="https://"
+              disableUnderline
+              fullWidth
+              autoFocus
+            />
+          </form>
+        )}
+    </div>
   );
+
+  function onBoldButtonMouseDown(event: any) {
+    event.preventDefault();
+    toggleFormat(editor, 'bold');
+  }
+
+  function onItalicButtonMouseDown(event: any) {
+    event.preventDefault();
+    toggleFormat(editor, 'italic');
+  }
+
+  function onUnderlinedButtonMouseDown(event: any) {
+    event.preventDefault();
+    toggleFormat(editor, 'underlined');
+  }
+
+  function onLinkButtonMouseDown(event: any) {
+    event.preventDefault();
+    insertLink(editor, 'https://www.google.com');
+  }
+
+  function toggleFormat(editor: any, format: any) {
+    Transforms.setNodes(
+      editor,
+      { [format]: isFormatActive(editor, format) ? null : true },
+      { match: Text.isText, split: true }
+    )
+  }
+  
+  function isFormatActive(editor: any, format: any) {
+    const [match]: any = Editor.nodes(editor, {
+      match: n => n[format] === true,
+      mode: 'all',
+    });
+    return !!match;
+  }
+
+  function insertLink(editor: any, url: string) {
+    if (editor.selection) {
+      wrapLink(editor, url);
+    }
+  }
+}
+
+export function wrapLink(editor: any, url: string) {
+  if (isLinkActive(editor)) {
+    unwrapLink(editor);
+  }
+
+  const { selection } = editor;
+  const isCollapsed = selection && Range.isCollapsed(selection);
+  const link = {
+    type: 'link',
+    url,
+    children: isCollapsed ? [{ text: url }] : [],
+  };
+
+  if (isCollapsed) {
+    Transforms.insertNodes(editor, link);
+  } else {
+    Transforms.setNodes(
+      editor,
+      link,
+      { match: Text.isText, split: true }
+    )
+    //Transforms.wrapNodes(editor, link, { split: true });
+    //Transforms.collapse(editor, { edge: 'end' });
+  }
+};
+
+function isLinkActive(editor: any) {
+  const [match]: any = Editor.nodes(editor, {
+    match: n => n.type === 'link',
+  });
+  return !!match;
+}
+
+function unwrapLink(editor: any) {
+  Transforms.unwrapNodes(editor, {
+    match: n => n.type === 'link'
+  });
 }
